8d99c03a825359a1cf72bef44fcb8abb374db3be
rkventures <kolipaka.ravi@gmail.com>
2025-07-22 15:34:35 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Start FastAPI backend (e.g., for /upload) on port 8000
 uvicorn app:api --host 0.0.0.0 --port 8000 &
-
-# Start Streamlit frontend on port 10000 (Render default)
 streamlit run app.py --server.port 10000 --server.enableCORS false
