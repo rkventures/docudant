7459d51bf49a2d9e4ca6a95358299a235f3239f0
rkventures <kolipaka.ravi@gmail.com>
2025-07-22 18:06:08 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 apt-get update && \
-apt-get install -y tesseract-ocr
+apt-get install -y tesseract-ocr && \
+pip install --upgrade pip && \
+pip install -r requirements.txt
 
+# Start FastAPI or Streamlit based on your app
 streamlit run app.py
+
 
