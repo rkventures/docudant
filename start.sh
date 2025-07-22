6ffdf944e4f4dccf23b1ffb3ba281482b29321a7
rkventures <kolipaka.ravi@gmail.com>
2025-07-22 15:55:43 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-uvicorn app:api --host 0.0.0.0 --port 8000 &
-streamlit run app.py --server.port 10000 --server.enableCORS false
+
+apt-get update && \
+apt-get install -y tesseract-ocr
+
+streamlit run app.py
+
